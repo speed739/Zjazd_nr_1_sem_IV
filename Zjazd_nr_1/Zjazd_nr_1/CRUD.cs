c01fe7984b302bbca34a3d756c043f0b87868e08
Grzesiek a <grzesiek7393223@o2.pl>
2020-03-23 18:37:58 +0100

    asd

---
+++ Zjazd_nr_1/Zjazd_nr_1/CRUD.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Zjazd_nr1_semIV
+{
+    class CRUD
+    {
+        public void Create(int id, string regionDescription, SqlConnection connection)
+        {
+            string sql = "INSERT INTO Northwind.dbo.Region(RegionID,RegionDescription) VALUES (@id,@regionName)";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@regionName", regionDescription);
+            command.Parameters.AddWithValue("@id", id);
+
+            int param = command.ExecuteNonQuery();
+            if (param > 0)
+            {
+                Console.WriteLine("Dodano");
+            }
+        }
+        public void Update(int id, string nowa_nazwa, SqlConnection connection)
+        {
+            string sql = "UPDATE Northwind.dbo.Region SET RegionDescription = @regionName WHERE RegionID=@id";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@regionName", nowa_nazwa);
+            command.Parameters.AddWithValue("@id", id);
+            int param = command.ExecuteNonQuery();
+
+            if (param > 0)
+            {
+                Console.WriteLine("Zmieniono");
+            }
+        }
+
+        public void Delete(int id, SqlConnection connection)
+        {
+            string sql = "DELETE FROM Northwind.dbo.Region WHERE RegionID=@id";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+            int param = command.ExecuteNonQuery();
+
+            if (param > 0)
+            {
+                Console.WriteLine("Usunieto");
+            }
+        }
+
+    }
+}
---
+++ Zjazd_nr_1/Zjazd_nr_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Zjazd_nr1_semIV
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString =
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;
+                    Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                    TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            using SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            string Polecenie_SQL = "SELECT * FROM Customers";
+            SqlCommand polecenie = new SqlCommand(Polecenie_SQL, connection);
+            SqlDataReader reader = polecenie.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["CompanyName"]);
+            }
+            reader.Close();
+
+            CRUD test = new CRUD();
+            Console.WriteLine("-------------------------");
+            test.Create(13, "slask", connection);
+            test.Update(13, "inny_slask", connection);
+            test.Delete(13, connection);
+            connection.Close();
+        }
+    }
+}
